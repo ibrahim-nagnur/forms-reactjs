3cb9da6bbf75daa27124181d2ed53e542368edf5
ibrahim-hn <144771988+ibrahim-hn@users.noreply.github.com>
2023-09-12 22:13:25 +0530

    initial commit

---
+++ src/Components/FormContainer.js
@@ -0,0 +1,10 @@
+import containerStyles from "./FormContainer.module.css"
+
+export function FormContainer() {
+    return (
+      <div className={containerStyles.red}>
+        <h1>React dynamic form implementation</h1>
+      </div>
+    );
+  }
+  
